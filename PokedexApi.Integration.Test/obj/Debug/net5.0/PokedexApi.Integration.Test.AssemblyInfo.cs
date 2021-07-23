af21e38753bcd1a703a69081011cf1305db7f72c
Joshua Osei <joo@contentguru.com>
2021-07-23 17:03:46 +0100

    published on windows machine

--- PokedexApi.Integration.Test/obj/Debug/net5.0/PokedexApi.Integration.Test.AssemblyInfo.cs
+++ PokedexApi.Integration.Test/obj/Debug/net5.0/PokedexApi.Integration.Test.AssemblyInfo.cs
@@ -11,7 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("PokedexApi, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "/Users/joshuaosei/source/JoshuaOsei-Pokedex-Api/PokedexApi", "PokedexApi.csproj", "0")]
+[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("PokedexApi, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "C:\\Users\\joo01\\source\\JoshuaOsei-Pokedex-Api\\PokedexApi", "PokedexApi.csproj", "0")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokedexApi.Integration.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
